d5778d65b22296d52dd39a45bac0d0e5ff0bc602
Artyom <artyom773@gmail.com>
2018-10-02 17:58:22 +0300

    add

--- deep_c/1/part1.c
+++ deep_c/1/part1.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stddef.h>
+#include <string.h>
 
-#define EN_SIZE 32
-#define RU_SIZE 32
+#include <assert.h>
+
+#define ERROR -1
+#define POSITION_OF_FIRST_CAPITAL_EN 65
+#define POSITION_OF_LAST_CAPITAL_EN 90
+#define POSITION_OF_FIRST_CAPITAL_RU 128
+#define POSITION_OF_LAST_CAPITAL_RU 159
+
+#define OFFSET 32
+#define BUFFER_SIZE 32
 
 int len(char* arr)
 {
@@ -12,82 +22,141 @@ int len(char* arr)
 	return size;
 }
 
-int function(char **arr, int arr_size, char ***save)
+int lowRegistr(char **arr, int arr_size, char ***save)
 {
-	// неверное кол-во строк 
-	if (arr_size < 1)
-	{
-		printf("[error]");
-		return 0;
-	}
-	// указатель на массив строк не задан
-	if (!arr)
-	{
-		printf("[error]");
-		return 0;
-	}
+	assert(arr != NULL);
+	assert(*arr != NULL);
+	assert(save != NULL);
+	assert(arr_size > 0);
 	
 	// выделим память под массив строк
 	*save = (char**)calloc(arr_size, sizeof(char*));
 	
 	// не удалось выделить память
-	if (!*save)
-	{
-		printf("[error]");
-		return 0;
+	if (!*save){
+		return ERROR;
 	}
 	
 	// размер строки
 	int row_size = 0;
-	
-	int j = 0;
 	
 	// цикл по строкам
 	for (int i = 0; i < arr_size; i++)
 	{
 		row_size = len(arr[i]);
 		
-		//Довыделим память непосредственно под строки
-		(*save)[i] = (char*)calloc(row_size, sizeof(char));
+		//Выделим память непосредственно под строки
+		(*save)[i] = (char*)calloc(row_size + 1, sizeof(char));
+		//(*save)[i][row_size] = 0;
 		
 		// Если не вышло
-		if (!(*save)[i])
-		{
+		if (!(*save)[i]){
 			// Очистим память строк
 			i--;
-			for (; i >= 0; i--)
-			{
+			for (; i >= 0; i--){
 				free((*save)[i]);
 			}
 			// и память массива строк
 			free(*save);
 			
-			printf("[error]");
-			return 0;
+			return ERROR;
+		}
+
+		//цикл по строке
+		for (int j = 0; j < row_size; j++)
+		{
+			if ((arr[i][j] >= POSITION_OF_FIRST_CAPITAL_RU) &&
+			(arr[i][j] <= POSITION_OF_LAST_CAPITAL_RU))
+			{
+				arr[i][j] = arr[i][j] + OFFSET;
+			} else if((arr[i][j]  >= POSITION_OF_FIRST_CAPITAL_EN)
+				&& (arr[i][j] <= POSITION_OF_LAST_CAPITAL_EN)){
+				arr[i][j] = arr[i][j] + OFFSET;
+			}
+			(*save)[i][j] = arr[i][j];
 		}
 		
-		j = 0;
-		//цикл по символам
-		for (char *ch = arr[i]; *ch != 0; ch++, j++)
-		{
-			if ((*ch > 126) && (*ch < 160))
-				*ch = *ch + RU_SIZE;
-			else if ((*ch > 64) && (*ch < 91))
-				*ch = *ch + EN_SIZE;
-			(*save)[i][j] = *ch;
-		}
 	}
 	return arr_size;
 }
 
+int my_getdelim(char **lineptr, int *n, int delimiter, FILE *stream)
+{
+	assert(lineptr != NULL);
+	assert(n != NULL);
+	assert(stream != NULL);
+	
+    int count = 0;
+    char *pb = NULL;
+    int c = 0;
+   
+    char *tmp = NULL;
+
+    if (!*lineptr)
+    {
+        tmp = realloc(*lineptr, *n);
+        if (!tmp)
+            return -1;
+        *lineptr = tmp;
+    }
+
+    pb = *lineptr;
+	
+    while ((c = fgetc(stream)) != EOF)// && c != delimiter)
+    {
+        if (count >= *n - 1)
+        {   
+            *n += BUFFER_SIZE;
+            tmp = realloc(*lineptr, *n);
+            if (!tmp)
+                return -1;
+
+            *lineptr = tmp;
+            pb = tmp + count;
+        }
+		if (c == delimiter)
+            break;
+        *pb = c;
+        pb++;
+        count++;
+    }
+    
+    if (count == 0)
+        return -1;
+
+    *pb = '\0';
+
+    return count;
+}
+
 int main(int argc, char* argv[])
 {
-	setlocale(LC_ALL, "Rus");
-	char** text;
-	int err = function(argv, argc, &text);
-	for (int i = 1; i < argc; i++)
-	{
-		printf("%s", text[i]);
-	}
+	setbuf(stdout,NULL);
+      
+   char **arr = (char**)calloc(BUFFER_SIZE, sizeof(char*));
+   int i = 0;
+   int max_size = BUFFER_SIZE;
+do
+{
+	arr[i] = (char*)calloc(BUFFER_SIZE, sizeof(char));
+}
+while (my_getdelim(&arr[i++], &max_size, '\n', stdin) != -1);
+   char** text = NULL;
+   int lines = lowRegistr(arr, i, &text);
+   if (lines == ERROR)
+	   printf("[error]");
+   else
+   {
+		for (int f = 0; f < lines; f++)
+		{
+			if (f != 0)
+				printf("\n");
+			printf("%s", text[f]);
+			free(arr[f]);
+			free(text[f]);
+		}
+   }
+	free(arr);
+	free(text);
 	return 0;
 }
